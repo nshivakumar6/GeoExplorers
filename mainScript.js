36ea75e67714b0a0e997c020b9fd919faa4c39c3
nik13793 <nik13793@esri.com>
2024-07-27 15:02:03 -0700

    added images and basic functionality

---
+++ mainScript.js
@@ -0,0 +1,175 @@
+﻿require([
+        "esri/config",
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer",
+        "esri/widgets/Editor",
+        "esri/widgets/Expand",
+        "esri/widgets/Search",
+        "esri/core/reactiveUtils",
+        "esri/popup/content/AttachmentsContent",
+        "esri/popup/content/TextContent"
+        ], function(esriConfig, Map, MapView, FeatureLayer, Editor, Expand, Search, reactiveUtils, AttachmentsContent, TextContent) {
+
+        // Function to create rating content
+        function ratingContent(feature) {
+        // Set how the attachments should display within the popup
+        const attachmentsElement = new AttachmentsContent({
+        displayType: "list"
+        });
+
+        const textElement = new TextContent();
+
+        const rating = feature.graphic.attributes.Rating;
+        let color;
+        if (rating >= 4) {
+        color = "green";
+        } else if (rating >= 2) {
+        color = "purple";
+        } else {
+        color = "red";
+        }
+
+        textElement.text = `The rating for this feature is <b><span style='color:${color}'>${rating}</span>/5.`;
+
+        return [textElement, attachmentsElement];
+        }
+
+        // Reference a feature layer to edit
+        const myPointsFeatureLayer = new FeatureLayer({
+        url: "https://services8.arcgis.com/LLNIdHmmdjO2qQ5q/arcgis/rest/services/HackthonProject_gdb/FeatureServer",
+        outFields: ["*"], // Ensure all fields are available for editing
+        popupTemplate: {
+        title: "Location: {place_name}",
+        content: ratingContent,
+        fieldInfos: [
+        { fieldName: "rating", label: "Rating" }
+        ],
+        actions: [{
+        title: "Edit",
+        id: "edit-this",
+        className: "esri-icon-edit"
+        }]
+        }
+        });
+
+        esriConfig.apiKey = "AAPKfc2bb44eed604a8fb65dfb72c87516b3YdztxXfTJmkRU-XaiQRV8hKNAwckTew2BfIFlC5mXBd-biav5bHtihYv178cmpyr";
+
+        const map = new Map({
+        basemap: "arcgis/topographic", // basemap styles service
+        layers: [myPointsFeatureLayer]
+        });
+
+        const view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [-117.18267, 34.0589],
+        zoom: 13
+        });
+
+        // Add the title to the view
+
+        // Search widget
+        const searchWidget = new Search({
+        view: view, 
+        popupEnabled: false
+        });
+
+        // Add the Search widget to an Expand widget
+        const searchExpand = new Expand({
+        view: view,
+        content: searchWidget,
+        expandIconClass: "esri-icon-search", // Optional, adds a search icon to the Expand widget
+        expandTooltip: "Search"
+        });
+
+        // Add the Search Expand widget to the top right corner of the view
+        view.ui.add(searchExpand, {
+        position: "top-right"
+        });
+
+        // Editor widget for adding new features
+        const addFeatureEditor = new Editor({
+        view: view,
+        icon: "plus"
+        });
+
+        // Add the Editor widget to an Expand widget with a plus icon
+        const addFeatureExpand = new Expand({
+        view: view,
+        content: addFeatureEditor,
+        expandIconClass: "esri-icon-plus", // Adds a plus icon to the Expand widget
+        expandTooltip: "Add Feature"
+        });
+
+        // Add the Expand widgets to the top right corner of the view, stacked
+        view.ui.add([addFeatureExpand, searchExpand], {
+        position: "top-right",
+        index: 0 // Ensures they are stacked in the order they are added
+        });
+
+        // Create the Editor with the specified layer and a list of field configurations
+        const editor = new Editor({
+        view: view,
+        
+        });
+
+        // Function to handle the "Edit feature" action
+        function editThis() {
+        if (!editor.activeWorkflow) {
+        view.popup.visible = false;
+        editor.startUpdateWorkflowAtFeatureEdit(view.popup.selectedFeature);
+        view.ui.add(editor, "top-right");
+        }
+
+        // Remove the editor widget from the display when the state of the editor's viewModel is "ready" and re-add the popup
+        reactiveUtils.when(
+        () => editor.viewModel.state === "ready",
+        () => {
+        view.ui.remove(editor);
+        view.openPopup({
+        fetchFeatures: true,
+        shouldFocus: true
+        });
+        }
+        );
+        }
+
+        // Event handler that fires each time an action is clicked
+        reactiveUtils.on(
+        () => view.popup,
+        "trigger-action",
+        (event) => {
+        if (event.action.id === "edit-this") {
+        editThis();
+        }
+        }
+        );
+
+        // Watch when the popup is visible
+        reactiveUtils.watch(
+        () => view.popup?.visible,
+        (event) => {
+        if (editor.viewModel.state === "editing-existing-feature") {
+        view.closePopup();
+        } else {
+        features = view.popup.features;
+        }
+        }
+        );
+
+        myPointsFeatureLayer.on("apply-edits", () => {
+        view.ui.remove(editor);
+        features.forEach((feature) => {
+        feature.popupTemplate = myPointsFeatureLayer.popupTemplate;
+        });
+        if (features) {
+        view.openPopup({
+        features: features
+        });
+        }
+        editor.viewModel.cancelWorkflow();
+        });
+
+
+        });
